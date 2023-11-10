003c97975ad71695b2e4332107e32bcc7bc0cae2
ianer-glitch <ianrepker@gmail.com>
2023-11-09 21:27:06 -0300

    configurações do projeto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ButtonBase from '@/components/atoms/buttonBase.vue'
+import TextFadeAnimation from '@/components/atoms/textFadeAnimation.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-container">
+    <TextFadeAnimation text="Olá," />
+
+    <textFadeAnimation text="Meu nome é Ian R. Malavazi" animation-duration="2s" />
+
+    <textFadeAnimation text="Sou desenvolvedor Web" animation-duration="3s" />
+
+    <ButtonBase class="movement" rounded>
+      <h3>Botão</h3>
+    </ButtonBase>
+  </div>
 </template>
+<style scoped>
+.home-container {
+  background-color: var(--bg-color);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 4rem;
+}
+.movement {
+  animation: moveButton 1s ease-in-out;
+}
+
+@keyframes moveButton {
+  0% {
+    transform: translateY(200px) rotate(0deg);
+  }
+  100% {
+    transform: translateY(0px) rotate(360deg);
+  }
+}
+
+@media (min-width: 300px) {
+}
+</style>
