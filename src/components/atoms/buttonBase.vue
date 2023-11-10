1bb720555a5bb6598a8c9af21e29d6d1703d666f
ianer-glitch <ianrepker@gmail.com>
2023-11-09 21:29:15 -0300

    configurações no projeto

---
+++ src/components/atoms/buttonBase.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const props = defineProps({
+  rounded: {
+    type: Boolean,
+    default: false
+  },
+  primary: {
+    type: Boolean,
+    default: true
+  },
+  secondary: {
+    type: Boolean,
+    default: true
+  }
+})
+</script>
+
+<template>
+  <button class="button" :class="{ primary: primary, secondary: secondary, rounded: rounded }">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  background-color: tomato;
+  padding: 1rem;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.primary {
+}
+
+.secondary {
+}
+.rounded {
+  border-radius: 5rem;
+}
+</style>
---
+++ src/components/atoms/textFadeAnimation.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: String,
+  animationDuration: {
+    default: '0.6s',
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="text-container">
+    <div class="flex items-center justify-start text-center">
+      <h1>{{ props.text }}</h1>
+    </div>
+  </div>
+</template>
+<style scoped>
+.text-container {
+  animation: expand v-bind('animationDuration') ease-in-out;
+  overflow: hidden;
+}
+
+@keyframes expand {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+</style>
