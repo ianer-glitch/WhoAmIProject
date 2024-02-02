e862512025cdedaf1ce219c64b5e5969bd79f0f9
ianer-glitch <ianrepker@gmail.com>
2024-02-01 21:54:37 -0300

    adição de validação para não quebrar caso o usuário tenha uma linguagem diferente no navegador

--- src/stores/translate.ts
+++ src/stores/translate.ts
@@ -3,6 +3,14 @@ import { defineStore } from 'pinia'
 import { LanguageEnum } from '@/enums/LanguageEnum'
 
 export const useTranslateStorage = defineStore('translate', () => {
-  const language = ref(navigator.language)
+  const browserLanguage = navigator.language
+  const arrayLanguages =  Object.values(LanguageEnum) as Array<string>
+  
+  let language = ref(LanguageEnum.english as string)
+  
+  if(arrayLanguages.includes(browserLanguage)){
+     language = ref(navigator.language)
+  }
+  
   return { language }
 })
