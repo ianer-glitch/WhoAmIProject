824cef6555be81d4fe27892012d9066f4c021c8a
ianer-glitch <ianrepker@gmail.com>
2024-02-15 22:30:55 -0300

    alterações na gramática do projeto

--- src/shared/projectListForShowcase.ts
+++ src/shared/projectListForShowcase.ts
@@ -4,8 +4,8 @@ import { LanguageEnum } from '@/enums/LanguageEnum'
 const projectListForShowcase: Array<ProjectShowcaseItem> = [
   {
     id: '0',
-    publishedLink: 'https://google.com',
-    repositoryLink: 'https://google.com',
+    publishedLink: 'https://ianrepkermalavazi.com.br',
+    repositoryLink: 'https://github.com/ianer-glitch/WhoAmIProject',
     tecnlogies: ['Vuejs', 'Typescript', 'HTML', 'CSS', 'Tailwind', 'PrimeVue'],
     imageName: 'portfolio-logo.png',
     readeableInformation: [
@@ -13,8 +13,10 @@ const projectListForShowcase: Array<ProjectShowcaseItem> = [
         languageName: LanguageEnum.english,
         bannerName: '',
         description: [
-          "This portfolio was created to show my skills as a developer!,It's all my years experiences included in one project. This app has a gret Front End Component usage and has the objective to show my coolest projects to everyone!.",
-          'The Application contains the possibility of choose Portuguese language or English language , light theme or dark theme and for all this features, it considers de user preference to start '
+          'This portfolio was created to show my developer skills !',
+          'It\'s is a compilation of all my experiences and a sucessfull Frontend implementation of atomic desing.',
+          'A user that acess the app can choose between Portuguese language or English Language, light theme or dark theme and it can automatically choose based on the person\'s preference.',
+          'My goal here is show my best projects to everyone!'
         ],
         name: 'My Portfolio'
       },
@@ -22,8 +24,10 @@ const projectListForShowcase: Array<ProjectShowcaseItem> = [
         languageName: LanguageEnum.brazilianPortuguese,
         bannerName: '',
         description: [
-          'Este portfólio foi criado para demonstrar minhas habilidades! Ele é um compilado de todas as  experiências que adquiri ao longo dos anos.O site por si, já é um belo exemplo de aplicação da Componentização do Front End, mas seu objetivo é tornar  meus melhores projetos visível todos!',
-          'A aplicação conta com tema Claro e tema  Escuro , linguagem Português e inglês. Além de considerar a preeferência  do usuário para selecionar previamente acada uma dessas opções.'
+          'Este portfólio foi criado para demonstrar minhas habilidades!',
+          'Ele é um compilado de todas as experiências que adquiri ao longo dos anos. Construído utilizando desing atômico,o site  é um belo exemplo de aplicação de componentização  no Frontend.',
+          'A aplicação conta com tema claro e tema  escuro , linguagem Português e Inglês. Além de considerar a preeferência  do usuário para selecionar previamente acada uma dessas opções.',
+          'O Objetivo aqui é tornar meus melhores projetos visíveis a todos!.'
         ],
         name: 'Meu Portfólio'
       }
